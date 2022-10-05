a6d6db9217910b74ab4696c563ad89a5f0edd75e
frankeka402 <garretonepet20@gmail.com>
2022-10-05 17:31:57 -0300

    agregando suma,resta y division

--- JS/script.js
+++ JS/script.js
@@ -18,9 +18,33 @@ function multiplicacion(num1, num2) {
     return m;
 
 }
-
 console.log(multiplicacion(num1, num2));
 //#endregion
+
+//#region 
+function division(num1, num2) {
+    let d = num1 / num2;
+    return d;
+}
+console.log(division(num1, num2));
+//#endregion
+
+//#region 
+function suma(num1, num2) {
+    let s = num1 + num2;
+    return s;
+}
+console.log(suma(num1, num2));
+//#endregion
+
+//#region 
+function resta(num1, num2) {
+    let r = num1 - num2;
+    return r;
+}
+console.log(resta(num1, num2));
+//#endregion
+
 
 //#region 
 let colores = ["azul", "violeta", "rosa"];
